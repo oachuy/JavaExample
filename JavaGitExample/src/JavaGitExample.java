b0ae3a61f9c9bbf421893078b2c4ab5f2e94e80a
Oscar <oachuy@gmail.com>
2012-02-08 23:21:10 -0500

    2da

--- JavaGitExample/src/JavaGitExample.java
+++ JavaGitExample/src/JavaGitExample.java
@@ -5,7 +5,7 @@ public class JavaGitExample {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("1er JavaExample");
+		System.out.println("2er JavaExample");
 
 	}
 
